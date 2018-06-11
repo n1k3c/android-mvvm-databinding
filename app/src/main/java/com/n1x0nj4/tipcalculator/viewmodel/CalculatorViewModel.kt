709d5d4b10e7ef54a1a8db99e9d797c5e1f325d4
Nikola Curilovic <ncurilovic@gmail.com>
2018-06-11 17:00:05 +0200

    Wire other inputs with viewmodel

--- app/src/main/java/com/n1x0nj4/tipcalculator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/n1x0nj4/tipcalculator/viewmodel/CalculatorViewModel.kt
@@ -1,10 +1,13 @@
 package com.n1x0nj4.tipcalculator.viewmodel
 
+import android.databinding.BaseObservable
 import android.util.Log
 import com.n1x0nj4.tipcalculator.model.Calculator
 import com.n1x0nj4.tipcalculator.model.TipCalculation
 
-class CalculatorViewModel(private val calculator: Calculator = Calculator()) {
+const val TAG = "CalculatorViewModel"
+
+class CalculatorViewModel(private val calculator: Calculator = Calculator()) : BaseObservable() {
 
     var inputCheckAmount = ""
 
@@ -13,13 +16,21 @@ class CalculatorViewModel(private val calculator: Calculator = Calculator()) {
     var tipCalculation = TipCalculation()
 
     fun calculateTip() {
-        Log.d("VM", "onClickFired")
+        Log.d(TAG, "onClickFired")
 
         val checkAmount = inputCheckAmount.toDoubleOrNull()
         val tipPct = inputTipPercentage.toIntOrNull()
 
         if (checkAmount != null && tipPct != null) {
+            Log.d(TAG, "CheckAmount: $checkAmount, TipPercentage: $tipPct")
             tipCalculation = calculator.calculateTip(checkAmount, tipPct)
+            clearInputs()
         }
+    }
+
+    private fun clearInputs() {
+        inputCheckAmount = "0.00"
+        inputTipPercentage = "0"
+        notifyChange()
     }
 }
